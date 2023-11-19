c6cf6c9687bdd88f2895ec5913ab535920316908
Sanjay Yadav <73985853+Rao-Sanjay@users.noreply.github.com>
2023-11-19 22:24:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Posts from './Components/Posts';
+import Profile from './Components/Profile';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Profile/>
+      <Posts/>
+    </>
+  );
+}
+
+export default App;
